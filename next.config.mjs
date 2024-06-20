9007203fbf92c17b35d46f0b5eaa6ce88ece4e85
Victor <onyemaechivictor2016@gmail.com>
2024-06-20 23:43:11 +0100

    tr

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@
 const nextConfig = {
     images: {
         domains:["cafeu.vercel.app", "img.freepik.com","colorlib.com","images.unsplash.com"], // Add more as needed.
-      },
+    },
+
+    eslint:{
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors. It's recommended to enable this option
+      // only temporarily until you've addressed all warnings.
+      ignoreDuringBuilds: true,
+    },
+   
 };
 
 export default nextConfig;
